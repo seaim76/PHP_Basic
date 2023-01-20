53c0d27f268a01612d28d8dfd5b3c26280406f03
Seaim <58206436+seaim76@users.noreply.github.com>
2023-01-20 19:41:28 +0600

    Operator

---
+++ operator.php
@@ -0,0 +1,59 @@
+<?php 
+
+// Add
+echo 8 + 5;
+
+// Minus
+
+echo 2 - 4;
+
+// divide
+
+echo 2 / 4;
+// multi
+
+echo 2 * 4;
+
+
+
+// power of 
+
+echo 2 ** 3
+
+
+// modul 
+
+echo 9 % 2;
+
+// comparision operator
+
+// equal or not equal
+
+var_dump1(2 == 3);
+var_dump(2 != 3);
+
+// identical || not identical
+
+var_dump (2 ==='2');
+
+var_dump (2 !=='2');
+
+
+// grater than || grater than equal
+
+var_dump(2 >= 2);
+var_dump(2 > 5);
+
+// less thean or less than equal
+var_dump(5 < 2);
+var_dump(2 <= 2);
+
+// Logical operator
+var_dump(true && true);
+var_dump(true && false);
+
+
+// or
+
+var_dump(true || false);
+?>
